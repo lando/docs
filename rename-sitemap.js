9f85b8f4122b88108a04ae14dbc8be027e8ad139
Alec Reynolds <alec+git@lando.dev>
2024-02-13 14:57:34 -0800

    Feed the linter.

--- rename-sitemap.js
+++ rename-sitemap.js
@@ -4,7 +4,7 @@ const path = require('path');
 const oldPath = path.join(__dirname, '.vitepress', 'dist', 'sitemap.xml');
 const newPath = path.join(__dirname, '.vitepress', 'dist', 'docs-sitemap.xml');
 
-fs.rename(oldPath, newPath, (err) => {
+fs.rename(oldPath, newPath, err => {
   if (err) {
     console.error('Error renaming sitemap.xml:', err);
     process.exit(1);
